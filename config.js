5a41df5d0ff1d49ae359887792fd5bf53a5244d9
Aka Mitch <newmitch@gmail.com>
2024-03-28 12:24:20 -0300

    key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-apiKey: "109df2ad-111f-4ce3-ab4a-0423c9101972",
+apiKey: "a6b9fcdb-7bb1-4d8e-b726-97704839cd0c",
 baseURL: "https://adult.ai.total-tube.com/v1",
 prompt: "Make description of porno sciene, in hardcore style, length 20-30 words using this clues:",
 inFile: "in.txt",
